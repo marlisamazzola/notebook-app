faab7f430840800a046d9f34558a8758ef0e6b60
marlisamazzola <marlisamazzola@outlook.it>
2026-05-27 15:10:14 +0200

    ultime modifiche

--- src/components/Button1.jsx
+++ src/components/Button1.jsx
@@ -1,4 +1,4 @@
-export default function Button2({ children, ...props }) {
+export default function Button1({ children, ...props }) {
   return (
     <button 
       className="px-4 py-2 text-xs md:text-base rounded-md font-bold active:scale-95 shadow-sm border bg-btn-bg border-btn-bg text-btn-text hover:bg-btn-bg-hover hover:border-btn-bg-hover hover:text-btn-text-hover" 
--- src/components/NoNotebookSelected.jsx
+++ src/components/NoNotebookSelected.jsx
@@ -8,10 +8,10 @@ export default function NoNotebookSelected({ onStartAddNotebook }) {
   return (
     <div className="mt-24 text-center w-[90%] md:w-2/3 mx-auto">
       <img src={notebook} alt="Quaderno" className="w-25 h-25 mx-auto mb-4" />
-      <h2 className="text-xl font-bold text-main-text my-4">
+      <h2 className="text-xl font-bold text-text-main my-4">
         Nessun quaderno selezionato
       </h2>
-      <p className="text-main-text mb-4">
+      <p className="text-text-main mb-4">
         Seleziona un quaderno o creane uno nuovo per iniziare a prendere appunti!
       </p> 
       <p className="mt-8">
